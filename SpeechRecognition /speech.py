ed67ec88cf8b6842aec2fc09c8e031c9a455c215
sashbhagav <sashreek_bhagav@hotmail.com>
2019-03-23 20:09:52 -0500

    wavefile

--- SpeechRecognition /speech.py
+++ SpeechRecognition /speech.py
@@ -2,6 +2,7 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
+
 with sr.Microphone() as source:
     print("speak now : ")
     audio = r.record(source, duration = 10)
@@ -11,3 +12,14 @@ with sr.Microphone() as source:
         print("you said :{} ".format(text))
     except:
         print("didnt recognize")
+
+#def recognize_speech(wavefile):
+#    with wavefile as source:
+#    print("listening : ")
+#    audio = r.record(source, offset=1)
+#    
+#    try:
+#        text = r.recognize_google(audio)
+#        print("you said :{} ".format(text))
+#    except:
+#        print("didnt recognize")
