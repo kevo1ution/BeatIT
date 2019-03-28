bd1fa89051f44435bb9337266d2dc5328dcf85d6
sashbhagav <sashreek_bhagav@hotmail.com>
2019-03-28 15:26:52 -0500

    just added length of track

--- server/flaskTests/analyzeAudio.py
+++ server/flaskTests/analyzeAudio.py
@@ -4,6 +4,7 @@ from flask import (
     Blueprint, request
 )
 import speech_recognition as sr
+import soundfile as sf
 r = sr.Recognizer()
 
 bp = Blueprint('analyzeAudio', __name__, url_prefix='/analyzeAudio')
@@ -14,12 +15,14 @@ def analyzeAudio():
     with open('audio.wav', 'wb') as infile:
         infile.write(request.data)
     src = sr.AudioFile('audio.wav')
+    f = sf.SoundFile('audio.wav')
+    print('seconds = {}'.format(len(f) / f.samplerate))
     with src as source:
         audio = r.record(source)
         print(type(audio))
         try:
             text = r.recognize_google(audio)
-            print("you said :{} ".format(text))
+            print("lyrics:{} ".format(text))
         except:
             print("didnt recognize")
     return('This is the analyze')
